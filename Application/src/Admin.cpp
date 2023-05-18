0ccb906242ae2c5e232ad06c15546cf3bc494b0a
Mahmoud <Mahmoud612.ashraf@gmail.com>
2023-05-18 06:35:12 +0300

    Admin class

---
+++ Application/src/Admin.cpp
@@ -0,0 +1,15 @@
+//
+// Created by MAHMOUD on 5/18/2023.
+//
+
+#include "Admin.h"
+
+#include <utility>
+#include "User.h"
+
+/** Constructors **/
+Admin::Admin(std::string name, std::string username, std::string password)
+:   User(std::move(name)),
+    username{std::move(username)},
+    password{std::move(password)}
+{}
---
+++ Application/src/Admin.h
@@ -0,0 +1,26 @@
+//
+// Created by MAHMOUD on 5/18/2023.
+//
+
+#ifndef INC_23_05_ADMIN_H
+#define INC_23_05_ADMIN_H
+
+#include "User.h"
+#include <string>
+
+class Admin : public User {
+public:
+    /** Constructors **/
+    Admin(std::string name,std::string username,std::string password);
+
+    /** Const Methods **/
+    std::string getUsername(){return username;};
+
+private:
+    /**  **/
+    std::string username;
+    std::string password;
+};
+
+
+#endif //INC_23_05_ADMIN_H
